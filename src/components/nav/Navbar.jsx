a179ea08c5b3cf7e75b68d86f31c16123d3ca29d
morris menanya <emmiemenz@gmail.com>
2025-10-16 01:50:40 +0100

    fix: replace image

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 // import { useRouter } from 'next/router'
@@ -7,10 +8,17 @@ import React from "react";
 const Navbar = () => {
   const router = useRouter();
   return (
-    <nav className="bg-black/50 shadow-md fixed w-full z-50 py-4">
+    <nav className="bg-black/50 shadow-md fixed w-full z-50 py-1">
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold text-blue-600">
-          <Link href="/">Gabify</Link>{" "}
+          <Link href="/" className="h-16 w-60 relative block overflow-hidden">
+            <Image
+              fill
+              src="/logo.PNG"
+              alt="LOGO"
+              className="object-contain scale-150 bg-white"
+            />
+          </Link>{" "}
         </h1>
         <ul className="hidden md:flex space-x-8 text-gray-200 font-medium">
           <li>
